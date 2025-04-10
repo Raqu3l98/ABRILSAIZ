cd0ce3e8cdbeec5aab2fc24f7629134036895a1e
dam <dam@203P15.mshome.net>
2025-04-10 11:29:51 +0200

    Segunda confirmación

---
+++ src/abrilsaiz/Clase2.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package abrilsaiz;
+
+/**
+ *
+ * @author dam
+ */
+public class Clase2 {
+    public void visualizar(){
+        System.out.println("Estoy en la rama de PRODUCCIÓN");
+        System.out.println("Mi nombre es Raquel Saiz Lozano");
+    }
+}
