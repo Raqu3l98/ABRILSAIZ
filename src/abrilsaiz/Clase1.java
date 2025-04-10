6ff068de5c7c583a6cb5a0a687f6eb36bc5f81b5
dam <dam@203P15.mshome.net>
2025-04-10 11:14:11 +0200

    Primer commit

---
+++ src/abrilsaiz/Clase1.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package abrilsaiz;
+
+/**
+ *
+ * @author dam
+ */
+public class Clase1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+      visualizarNombre();
+    }
+     public static void visualizarNombre(){
+           System.out.println("Raquel Saiz Lozano");
+       }
+}
